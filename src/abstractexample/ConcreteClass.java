d485b2209de62629b4dc1249eea331977586f3e2
MOHAMMAD ABDUL RAB <153599435+Mohammadabdulrab@users.noreply.github.com>
2025-01-04 23:30:34 +0530

    Update ConcreteClass.java

--- src/abstractexample/ConcreteClass.java
+++ src/abstractexample/ConcreteClass.java
@@ -6,4 +6,8 @@ public class ConcreteClass extends AbstractClass{
     public void demo1() {
         System.out.println("Abstract method implementation");
     }
+
+    public void demo3(){
+        System.out.println("My name is Mohammad Abdul Rab.");
+    }
 }
